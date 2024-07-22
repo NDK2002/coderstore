d5b65483cb0ea5ed15359988b0920b41307bcd10
NDK <ndk@1200@gmail.com>
2024-07-23 00:02:19 +0700

    final app and clear all feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
+import ThemeProvider from "./contexts/ThemeProvider";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
